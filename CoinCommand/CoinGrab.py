340c467b67bea7e27735a98d2dc550f7ca8340d2
Gavin Goodship <msgoodship1@gmail.com>
2018-01-23 13:50:51 +0000

    Update CoinGrab.py

--- CoinCommand/CoinGrab.py
+++ CoinCommand/CoinGrab.py
@@ -6,13 +6,7 @@ class CoinGrabInformation(object):
 	"""class which gets coin information such as volume , price
 	algorithm information difficulty rate etc"""
 	
-	def __init__(self,coin,currency="GDP"):
-		self.coin = coin
-		self.volume = None
-		self.change = None
-		self.currency = currency
 	
-	def get_Coin_Information(self):
-		pass
+	pass
 		
 	
