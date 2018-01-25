44b5fd141f6dd5d33ea3951a0fd0688768a2e9eb
gavin <msgoodship1@gmail.com>
2018-01-25 21:48:46 +0000

    change to cp doc string

--- CoinCommand.py
+++ CoinCommand.py
@@ -9,7 +9,7 @@ def cli():
 @click.option('--price' , help = 'gets price of coin')
 @click.argument('coin') 
 def cp(coin,price):
-	"""gets coin price in usd as default"""
+	"""gets coin price """
 	url = "https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=%s"%(coin,price)
 	request_To_Url = requests.get(url)
 	click.echo(formattingUnicodeCurrency(request_To_Url.text,price))
@@ -24,5 +24,8 @@ def formattingUnicodeCurrency(request,price):
 	if(price =='EUR'):
 		return eur+str(unicode_format[price])
 
-
-
+@cli.command()
+@click.argument(coin)
+def algo(coin):
+	""" gets the coin implementation algorithm"""
+	pass
