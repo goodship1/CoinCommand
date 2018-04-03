46f144a0fc151ed57f400ebaaba3c1a6ed0246df
goodship1 <msgoodship1@gmail.com>
2018-04-03 14:26:31 +0100

    renamin conventions and adding get_social and error handling

--- CoinCommand.py
+++ CoinCommand.py
@@ -11,13 +11,18 @@ def cli():
 def coin_Doesnt_exist():
 	return "Coin Doesnt Exist"
 
+
+def connection_Error():
+    #error handling to handle http 400 status
+    return "no connection"
+
 @cli.command()
 @click.option('--currency',help = 'currency of coin')
 @click.argument('coin')
 def cp(coin,currency):
 	"""gets coin price """
 	try:
-		click.echo(cp_Request_To_Url(coin,currency))
+		click.echo(cp_Request_To_url(coin,currency))
 	except Exception as err:
 		print("Coin or Currency Doesnt Exist")
 
@@ -33,13 +38,16 @@ def cp_Request_to_Url(coin,price):
 
 
 def formatting_Unicode_currency(request,price):
+
+    """formatting unicode currency formats the json data to get price"""
+
 	unicode_Format = ast.literal_eval(request)
 	euro = u'\u20ac'
 	if(price == 'USD'):
 		return '$'+str(unicode_Format[price])
 	if(price =='EUR'):
 		return euro+str(unicode_Format[price])
-    #todo add support to for £
+    #Todo add support to for £
 
 
 @cli.command()
@@ -48,18 +56,18 @@ def mined(coin):
 	try:
 		click.echo(mined_Request_To_Url(coin))
 	except Exception as err:
-		print(coin_Doesnt_Exist())
+		print(coin_doesnt_Exist())
 
 
 def mined_Request_to_Url(coin):
 	url = 'https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=%s&tsym=USD'%coin
-	request_To_Url = requests.get(url)
-	return formatting_Unicode_mined(request_To_Url.text)
+	request_To_url = requests.get(url)
+	return formatting_Unicode_mined(request_To_url.text)
 
 
 def formatting_Unicode_mined(request):
-	formatting_Mined_Information = ast.literal_eval(request)
-	return formatting_Mined_Information['Data']['TotalCoinsMined']
+	formatting_Mined_information = ast.literal_eval(request)
+	return formatting_Mined_information['Data']['TotalCoinsMined']
 
 
 
@@ -71,22 +79,35 @@ def algo(coin):
 	try:
 		click.echo(algo_request_To_Url(coin))
 	except Exception as err:
-		print(coin_Doesnt_Exist())
+		print(coin_Doesnt_exist())
 
 
 
-def algo_request_To_Url(coin):
+def algo_Request_to_Url(coin):
 		url = 'https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=%s&tsym=USD'%coin
 		request_To_Url =  requests.get(url)
-		return formatting_Unicode_CoinSnapShot(request_To_Url.text)
+		return formatting_Unicode_coinsnapshot(request_To_Url.text)
+
+
+
+
+def formatting_Unicode_coinsnapshot(request):
+	formatting_Of_coinsnapshot = ast.literal_eval(request)
+	return formatting_Of_coinsnapshot['Data']['Algorithm']
+
+
 
 @click.command()
+@click.argument('socialmedia')
 def get_Social():
 	pass
     #todo allow the user to get social
 
 
 
-def formatting_Unicode_CoinSnapShot(request):
-	formatting_Of_CoinSnap_Shot = ast.literal_eval(request)
-	return formatting_Of_CoinSnap_Shot['Data']['Algorithm']
+def request_to_Social():
+    #funcution which gets socail request formatting_Mined_information
+
+
+def formatting_social_Unicode():
+    pass
