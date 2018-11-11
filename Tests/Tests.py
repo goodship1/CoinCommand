405e2f0427677e5b4a1e564d1a26265cb943bb2a
goodship1 <msgoodship1@gmail.com>
2018-11-11 21:35:05 +0000

    fixing tests naming conventions

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -11,11 +11,11 @@ euro = 'EUR'
 dollar_Sign = '$'
 
 
-def test_Algo_Return_Correct_Value():
+def test_Algo_return_Correct_value():
     assert CoinCommandTest.algo(btc) == "SHA256" 
 
 
-def test_Cp_Dollar_Return():
+def test_Cp_dollar_Return():
 	url = "https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=%s"%(btc,dollar)
 	request = requests.get(url)
 	cp_Return = str(CoinCommandTest.formatting_Unicode_Currency(request.text,dollar))
@@ -36,19 +36,19 @@ def test_of_Coin_NotPresent():
 	
 	
 
-def test_Mined_Return_Float():
+def test_mined_Return_float():
 	"""test to make sure mined returns the correct data type"""
 	assert type(CoinCommandTest.mined(btc)) == float
 	
 
-def test_FormattingUnicodeCurrency():
+def test_Formatting_unicode_Currency():
 	url = "https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=%s"%(btc,dollar)
 	request_To_Url = requests.get(url)
 	return_Currency = CoinCommandTest.formatting_Unicode_Currency(request_To_Url.text,"USD")
 	assert(type(return_Currency)) == str
 
 
-def test_FormattingUnicodeMined():
+def test_Formatting_unicode_Mined():
 	url = 'https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=%s&tsym=USD'%btc
 	request_To_Url = requests.get(url)
 	
@@ -61,13 +61,13 @@ def test_Algo_Incorrect_Coin():
 	assert(algo_return) == 	error
 	
 	
-def test_Mined_Incorrecnt_Coin():
+def test_mined_Incorrect_coin():
 	mined_Return = CoinCommandTest.mined(incorrect)
 	error = CoinCommandTest.coin_Doesnt_Exist()
 	assert(mined_Return) == error
 	
 	
-def test_Formatting_Unicode_CoinSnapShot():
+def test_Formatting_unicode_Coinsnapshot():
 	url = 'https://www.cryptocompare.com/api/data/coinsnapshot/?fsym=%s&tsym=USD'%btc
 	request_To_Url = requests.get(url)
 	return_CoinSnapShot = CoinCommandTest.formatting_Unicode_CoinSnapShot(request_To_Url.text)
